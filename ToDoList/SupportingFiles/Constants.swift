814453c43301ce839b175242671a1d66051ddbc5
Manpreet Singh <manpreet.singh@cineplex.com>
2020-02-27 15:55:52 -0500

    adding category view controller

---
+++ ToDoList/SupportingFiles/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ToDoList
+//
+//  Created by Manpreet Singh on 2020-02-27.
+//  Copyright © 2020 Manpreet Singh. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let categoryCellIdentifier = "categoryCell"
+}
