7db71dba947d558b7b080bd1cfd553af44770a48
Manpreet Singh <manpreet.singh@cineplex.com>
2020-02-28 18:10:26 -0500

    adding persistence service

---
+++ ToDoList/Services/PersistenceService.swift
@@ -0,0 +1,43 @@
+//
+//  PersistenceService.swift
+//  ToDoList
+//
+//  Created by Manpreet Singh on 2020-02-28.
+//  Copyright © 2020 Manpreet Singh. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PersistenceService {
+
+    static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    // MARK: - Core Data stack
+
+    static var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ToDoList")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    static func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
